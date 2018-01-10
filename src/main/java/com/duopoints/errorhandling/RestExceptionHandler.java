0da2eed4dab2359f350f54e030f2783a4b96ca08
Bahram Malaekeh <bahram.malaekeh@gmail.com>
2018-01-10 15:14:28 +0000

    - Added new Exceptions to the exception handler.

--- src/main/java/com/duopoints/errorhandling/RestExceptionHandler.java
+++ src/main/java/com/duopoints/errorhandling/RestExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Collections;
+
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
@@ -15,5 +17,15 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(NullResultException.class)
     protected ResponseEntity handleNullResultException() {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    protected ResponseEntity handleConflictException(ConflictException ex) {
+        return new ResponseEntity<>(Collections.singletonMap("conflict", ex.conflictReason), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(NoMatchingRowException.class)
+    protected ResponseEntity handleNoMatchingRowException(NoMatchingRowException ex) {
+        return new ResponseEntity<>(Collections.singletonMap("no_match_found", ex.matchNotFoundCause), HttpStatus.NOT_FOUND);
     }
 }
